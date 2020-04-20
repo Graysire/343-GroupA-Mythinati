c0e792faa487b6d50f0076e687802c9f8b48720d
Graysire <graysire@dyrix.net>
2020-04-20 16:42:43 -0700

    Added Unity project with photon Pun asset package

---
+++ Mythinati/Assets/Scripts/Card.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//any card that can be part of a deck
+public class Card : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Mythinati/Assets/Scripts/Group.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//a card that f
+public class Group : Card
+{
+    //power of the card usable for attacks
+    int power;
+    //power of the card usable for iaidng attacks
+    int transferablePower;
+    //how much is added to the treasury every turn
+    int income;
+    //how much currency this card has stored
+    int treasury;
+    //the directions this card can connect to
+    bool[] connectingDirections = new bool[4];
+    //the groups this card isconnected to
+    Group[] connectingGroups = new Group[4];
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
