8b29fc96e5bad25d9fc29f944747fd8b757516f3
Graysire <graysire@dyrix.net>
2020-05-03 15:52:27 -0700

    Added Placeholder BoardManager and Player classes

---
+++ Mythinati/Assets/Scripts/Game Scripts/BoardManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//manages all decks and players within the game
+public class BoardManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Mythinati/Assets/Scripts/Game Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int actionsRemaining;
+    public Group centralGroup;
+    public List<Card> specialCards = new List<Card>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
